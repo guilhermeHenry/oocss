25a86dca2e512d6657acf7d6a9855a3ee2663511
guilhermeHenry <gui2037@live.com>
2019-12-31 16:28:28 -0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-// buttons
+import select from './node_modules/select/app.js';
 
+let textarea = document.getElementById('textarea');
 
+// getstyle computed
+let styleCumputed = window.getComputedStyle(textarea);
+let style = {
+	line: parseInt(styleCumputed.getPropertyValue('line-height')),
+	pTop: parseInt(styleCumputed.getPropertyValue('padding-top')),
+	pBottom: parseInt(styleCumputed.getPropertyValue('padding-bottom'))
+}
 
+let resetRows = textarea.hasAttribute('rows') ? parseInt(textarea.rows) : 1;
+
+function resize() {
+	reset();
+	let s = textarea.scrollHeight;
+		s -= style.pTop;
+		s -= style.pBottom;
+
+	let rows = s / style.line;
+	textarea.rows = rows;
+}
+
+function reset() {textarea.rows = resetRows;}
+
+textarea.addEventListener('input', resize);
+resize();
 
 
 
